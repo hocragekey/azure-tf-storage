9704512d5f4c058b37375fa1f94e3ebf74d090f2
James Lebonitte <jlebonitte@gmail.com>
2018-08-19 19:26:36 -0400

    initial k8s cluster code committed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# azure-tf-storage
+# Terraform IaC for Spark on Kubernetes Experiment
+
+This IaC is for standing up a simple Big Data workflow environment to experiment submitting spark jobs on top of Kubernetes.
--- main.tf
+++ main.tf
@@ -1,4 +1,32 @@
-resource "azurerm_resource_group" "lebweb-storage-rg" {
- name = "lebweb-storage-rg"
- location = "eastus"
+resource "azurerm_resource_group" "spark-k8s" {
+ name = "${var.k8s_resource_group_name}"
+ location = "${var.location}"
 }
+resource "azurerm_kubernetes_cluster" "spark-k8s" {
+  name                = "${var.cluster_name}"
+  location            = "${azurerm_resource_group.spark-k8s.location}"
+  resource_group_name = "${azurerm_resource_group.spark-k8s.name}"
+  dns_prefix          = "${var.dns_prefix}"
+
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = "${file("${var.ssh_public_key}")}"
+    }
+  }
+
+  agent_pool_profile {
+    name            = "default"
+    count           = "${var.agent_count}"
+    vm_size         = "Standard_A0"
+    os_type         = "Linux"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = "${var.client_id}"
+    client_secret = "${var.client_secret}"
+  }
+}
+
